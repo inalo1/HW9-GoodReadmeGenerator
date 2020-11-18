803b1b0c8c638c352a7fc170dc65971cf49c5640
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2020-11-18 14:21:15 -0600

    successfully generated a readme

---
+++ Develop/README.md
@@ -0,0 +1,28 @@
+## Project Title 
+    readme
+
+    # Description
+    undefined
+    
+    # Table of Content 
+
+    # Installation Instructions
+    copy/paste
+    
+    # Usage Information
+    add info
+    
+    # Contribution Guidelines
+    asdads
+    
+    # Test Instructions
+    n/a
+    
+    # License
+    GNU AGPLv3
+    
+    # Information
+    inalo1
+    nalo.ivielle13@gmail.com
+        
+    
--- Develop/index.js
+++ Develop/index.js
@@ -1,102 +1,95 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
-const questions = inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'Project Title:',
-            name: 'title',
-        },
-        {
-            type: 'input',
-            message: 'Project Description:',
-            name: 'decription',
-        },
-        {
-            type: 'input',
-            message: 'Installation Instructions:',
-            name: 'installation-instructions',
-        },
-        {
-            type: 'input',
-            message: 'Usage Information:',
-            name: 'usage-information',
-        },
-        {
-            type: 'input',
-            message: 'Contribution Guidelines:',
-            name: 'contribution-guidelines',
-        },
-        {
-            type: 'input',
-            message: 'Test Instructions:',
-            name: 'test-instructions',
-        },
-        {
-            type: 'list',
-            message: 'Choose a License',
-            choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", 
-            "Apache License 2.0", "MIT License","Boost Software License 1.0", "The Unlicense"],
-            name: 'license',
-        },
-        {
-            type: 'input',
-            message: 'What is your Github username?',
-            name: 'github-username',
-        },
-        {
-            type: 'input',
-            message: 'What is your e-mail address?',
-            name: 'e-mail',
-        },
-    ])
-    .then((response) => {
-        console.log(response)
+const userInput = () =>
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'Project Title:',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Project Description:',
+        name: 'decription',
+    },
+    {
+        type: 'input',
+        message: 'Installation Instructions:',
+        name: 'installationInstructions',
+    },
+    {
+        type: 'input',
+        message: 'Usage Information:',
+        name: 'usageInformation',
+    },
+    {
+        type: 'input',
+        message: 'Contribution Guidelines:',
+        name: 'contributionGuidelines',
+    },
+    {
+        type: 'input',
+        message: 'Test Instructions:',
+        name: 'testInstructions',
+    },
+    {
+        type: 'list',
+        message: 'Choose a License',
+        choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", 
+        "Apache License 2.0", "MIT License","Boost Software License 1.0", "The Unlicense"],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'githubUsername',
+    },
+    {
+        type: 'input',
+        message: 'What is your e-mail address?',
+        name: 'eMail',
+    },
+]);
 
-        let readmeTemplate =
-        `## Project Title
-        ${response.title}
+const generateREADME = (response) => 
 
-        # Description
-        ${response.description}
+    `## Project Title 
+    ${response.title}
+
+    # Description
+    ${response.description}
+    
+    # Table of Content 
+
+    # Installation Instructions
+    ${response.installationInstructions}
+    
+    # Usage Information
+    ${response.usageInformation}
+    
+    # Contribution Guidelines
+    ${response.contributionGuidelines}
+    
+    # Test Instructions
+    ${response.testInstructions}
+    
+    # License
+    ${response.license}
+    
+    # Information
+    ${response.githubUsername}
+    ${response.eMail}
         
-        # Table of Content 
+    `;
+   
+userInput()
+.then((response) => writeFileAsync('README.md', generateREADME(response)))
+.then(() => console.log('Successfully wrote to README.md'))
+.catch((err) => console.error(err));
+    
 
-        # Installation Instructions
-        ${response.installation-instructions}
-        
-        # Usage Information
-        ${response.usage-information}
-        
-        # Contribution Guidelines
-        ${response.contribution-guidelines}
-        
-        # Test Instructions
-        ${response.test-instructions}
-        
-        # License
-        ${response.license}
-        
-        # Information
-        ${response.github-username}
-        ${response.e-mail}
-         
-        ` 
-
-
-    })
-
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
