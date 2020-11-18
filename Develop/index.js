1ab929fd02b3dafd348d6c5a6178e8c83fd418de
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2020-11-17 20:26:18 -0600

    added initial questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,58 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-];
+// array of questions for user
+const questions = inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Project Title:',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Project Description:',
+            name: 'decription',
+        },
+        {
+            type: 'input',
+            message: 'Installation Instructions:',
+            name: 'installation-instructions',
+        },
+        {
+            type: 'input',
+            message: 'Usage Information:',
+            name: 'usage-information',
+        },
+        {
+            type: 'input',
+            message: 'Contribution Guidelines:',
+            name: 'contribution-guidelines',
+        },
+        {
+            type: 'input',
+            message: 'Test Instructions:',
+            name: 'test-instructions',
+        },
+        {
+            type: 'list',
+            message: 'Choose a License',
+            choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", 
+            "Apache License 2.0", "MIT License","Boost Software License 1.0", "The Unlicense"],
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: 'What is your Github username?',
+            name: 'github-username',
+        },
+        {
+            type: 'input',
+            message: 'What is your e-mail address?',
+            name: 'e-mail',
+        },
+    ]);
+
 
 // function to write README file
 function writeToFile(fileName, data) {
